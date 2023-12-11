34a3bc0b7811a53300f9ca5a7b21da67cb31a54d
Dhruv Dhayal <137479629+BlockNotes-4515@users.noreply.github.com>
2023-12-11 10:51:31 +0530

    Added download.js

---
+++ download.js
@@ -0,0 +1,6 @@
+var imageUrls = [];
+har.log.entries.forEach(function (entry) {
+  if (entry.response.content.mimeType.indexOf("image/") !== 0) return;
+  imageUrls.push(entry.request.url);
+});
+console.log(imageUrls.join('\n'));
